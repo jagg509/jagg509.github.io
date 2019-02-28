eb3c17dd33ad5ace7b4e87ba4f5b519c0563b455
jagg509 <gon15018@byui.edu>
2019-02-27 22:12:13 -0600

    Week 8 updates

---
+++ cit230/lesson-8/scripts/activePage.js
@@ -0,0 +1,12 @@
+
+var menu = document.getElementById("directory")
+
+let pages = menu.getElementsByClassName("page");
+
+for (var i = 0; i < pages.length; i++) {
+    pages[i].addEventListener("click", function() {
+    var current = document.getElementsByClassName("active");
+    current[0].className = current[0].className.replace(" active", "");
+    this.className += " active";
+    });
+  }
