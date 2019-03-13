2c0ffca54fea42cf01023333ef4201665a909922
jagg509 <gon15018@byui.edu>
2019-03-12 19:54:12 -0600

    API 1

--- cit230/lesson-10/scripts/weatherapi.js
+++ cit230/lesson-10/scripts/weatherapi.js
@@ -1,15 +1,13 @@
-var weatherObj = new XMLHttpRequest();
-weatherObj.open('GET', 'https://api.openweathermap.org/data/2.5/forecast?id=5604473&units=imperial&APPID=ddfec18fe14556473c9c515e0dfebb7a',true);
+let weatherObj = new XMLHttpRequest();
+weatherObj.open('GET', 'https://api.openweathermap.org/data/2.5/forecast?id=5604473&units=imperial&APPID=ddfec18fe14556473c9c515e0dfebb7a', true);
 
 weatherObj.send();
 
 weatherObj.onload = function () {
-    
-    var jsonRespObj = JSON.parse(weatherObj.responseText);
+
+    let jsonRespObj = JSON.parse(weatherObj.responseText);
 
     console.log(jsonRespObj);
 
-    document.getElementById("current-temp").innerHTML = jsonRespObj.main.temp;
-
-
+    document.getElementById('current-temp').innerHTML = jsonRespObj.main.temp;
 }
